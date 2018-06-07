228921bf7c19f62a8f59ea924b26aed48fe917d7
Jocelyn <jocyroman@gmail.com>
2018-06-07 15:36:43 -0600

    training ready

--- flappy.py
+++ flappy.py
@@ -1,40 +1,30 @@
-import matplotlib.pyplot as plt
-
-import numpy as np
-import cv2
-
-from ple.games.flappybird import FlappyBird
-from ple import PLE
-
-# Imports for Gym Flappy Bird
+import argparse
 import gym
 import gym_ple
+import numpy as np
+from itertools import count
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
-for i in range(10):
-        ob = env.reset()
-        while True:
-            action = env.action_space.sample()
-            screen = env.render(mode='rgb_array')
-            ob, reward, done, b = env.step(action)
-            print(type(ob), type(reward), type(done), type(b))
-            print(ob, reward, done, b)
-            if done:
-                plt.figure()
-                plt.imshow(screen)
-                plt.title('Example extracted screen')
-                plt.show()
-                break
-from itertools import count
-
 from torch.distributions import Categorical
 
+parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
+parser.add_argument('--gamma', type=float, default=1.58, metavar='G',
+                    help='discount factor (default: 0.99)')
+parser.add_argument('--seed', type=int, default=543, metavar='N',
+                    help='random seed (default: 543)')
+parser.add_argument('--render', action='store_true',
+                    help='render the environment')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='interval between training status logs (default: 10)')
+args = parser.parse_args()
 
 env = gym.make('FlappyBird-v0')
+env.seed(args.seed)
+torch.manual_seed(args.seed)
 
 def visualize_image(x, title):
     # Based on: https://www.quora.com/How-can-l-visualize-cifar-10-data-RGB-using-python-matplotlib\
@@ -57,90 +47,63 @@ def pre_processing(raw_image):
     # Delete floor
     raw_image = np.delete(raw_image, range(404, 512), 1)
     print(raw_image.shape)
-    visualize_image(raw_image, "TEST")
 
     return raw_image
 
 def preprocessing(raw_image):
     imgData = np.asarray(raw_image)
     thresholdedData = (imgData > 200) * 1.0
-    visualize_image(raw_image, "TEST")
-
-
-from PIL import Image
-
-im = Image.open('test2.png')
-im = im.convert('L')
-#preprocess(np.array(im))
+    #visualize_image(raw_image, "TEST")
+    return thresholdedData
 
 
 
 class Policy(nn.Module):
     def __init__(self):
         super(Policy, self).__init__()
-        self.affine1 = nn.Linear(1, 512)
-        self.affine2 = nn.Linear(512, 2)
-
-        self.saved_log_probs = []
-        self.rewards = []
-
-    def forward(self, x):
-        x = F.relu(self.affine1(x))
-        action_scores = self.affine2(x)
-        return F.softmax(action_scores, dim=1)
-print("hi",np.asarray(im).shape)
-x=pre_processing(np.array(im))
-x = torch.from_numpy(x).float().unsqueeze(0)
-print(x.dim())
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = torch.nn.Conv2d(3, 7, kernel_size=5, stride=1)
-        self.conv2 = torch.nn.Conv2d(7, 7, kernel_size=5, stride=1)
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        #self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = torch.nn.Linear(128, 2)  # fully connected
+        #self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        #elf.bn3 = nn.BatchNorm2d(32)
+        self.head = nn.Linear(672, 2)
+
         self.saved_log_probs = []
         self.rewards = []
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1,128)
-        x = self.fc1(x)
-
+        x = F.relu(F.max_pool2d(self.conv3(x),2))
+        x = x.view(-1, 672)
+        x= self.head(x)
         return F.softmax(x, dim=1)
 
 
-policy = Net()
+
+policy = Policy()
 optimizer = optim.Adam(policy.parameters(), lr=1e-2)
 eps = np.finfo(np.float32).eps.item()
-gamma = 0.99
-log_interval = 10
-print(policy.forward(x))
-return cv2.resize(cv2.cvtColor(self.obs, cv2.COLOR_RGB2GRAY), (80, 80))
+
 
 def select_action(state):
-    #state = torch.from_numpy(state).float().unsqueeze(0)
-
-    #state = pre_processing(state)
-    state = pre_processing(np.array(state))
-    state = torch.from_numpy(x).float().unsqueeze(0)
+    state = state.transpose((2, 0, 1))
+    state = preprocessing(state)
+    state = torch.from_numpy(state).float().unsqueeze(0)
     probs = policy(state)
-
     m = Categorical(probs)
-    action = m.sample()
+    action = probs.argmin()
     policy.saved_log_probs.append(m.log_prob(action))
-    print(action.item)
-
-    return 0
+    return action.item()
 
 def finish_episode():
     R = 0
     policy_loss = []
     rewards = []
     for r in policy.rewards[::-1]:
-        R = r + gamma * R
+        R = r + args.gamma * R
         rewards.insert(0, R)
     rewards = torch.tensor(rewards)
     rewards = (rewards - rewards.mean()) / (rewards.std() + eps)
@@ -154,29 +117,47 @@ def finish_episode():
     del policy.saved_log_probs[:]
 
 
+def saveModel(epoch):
+    state = {
+        'epoch': epoch,
+        'state_dict': policy.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }
+    torch.save(state, "save.txt")
+
+def loadModel():
+    state = torch.load("save.txt")
+    policy.load_state_dict(state['state_dict'])
+    optimizer.load_state_dict(state['optimizer'])
+
+
 def main():
     running_reward = 10
     for i_episode in count(1):
         state = env.reset()
-        for t in range(10000):  # Don't infinite loop while learning#512
+        for t in range(10000):  # Don't infinite loop while learning
             action = select_action(state)
             state, reward, done, _ = env.step(action)
-            #env.render()
+            if args.render or True:
+                env.render()
             policy.rewards.append(reward)
             if done:
                 break
 
         running_reward = running_reward * 0.99 + t * 0.01
+        print(running_reward)
         finish_episode()
-        if i_episode % log_interval == 0:
+        if i_episode%5==0:
+            saveModel(i_episode)
+        if i_episode % args.log_interval == 0:
             print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(
                 i_episode, t, running_reward))
-        if running_reward > env.spec.reward_threshold:
+        if running_reward > 50:  # env.spec.reward_threshold:
             print("Solved! Running reward is now {} and "
                   "the last episode runs to {} time steps!".format(running_reward, t))
             break
 
-#main()
-
+if __name__ == '__main__':
+    main()
 
 
